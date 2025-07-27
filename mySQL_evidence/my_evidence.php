baad6a3b0effd75cb0e1b3a4a67eb1c42baa14e1
mahidulkabir <mahidulkabir8@gmail.com>
2025-07-27 12:57:11 +0600

    added product info table

--- mySQL_evidence/my_evidence.php
+++ mySQL_evidence/my_evidence.php
@@ -33,9 +33,12 @@ if(isset($_POST["submit_btn_prod_tble"])){
 $p_name = $_POST["prod_name"];
 $p_price = $_POST["prod_price"];
 $manu_id = $_POST["manufec_id"];
-
 $conn-> query("call add_product_info('$p_name','$p_price','$manu_id')");
+};
 
+if(isset($_POST["delmanufact"])){
+    $m_id = $_POST["manufac"];
+    $conn->query("DELETE from manufacturer_info WHERE id='$m_id'");
 };
 
 ?>
@@ -77,17 +80,59 @@ while(list($m_id,$m_name) = $manufec_id->fetch_row() ){
     echo "<option value='$m_id'>$m_name </option>";
 };
 ?>
-
-
 </select> <br><br>
-
-
-
 <input type="submit" value="Submit" name="submit_btn_prod_tble">
-
 </form>
 
 
+<!-- after delete trigger -->
+<form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+	
+		
+		<label for="manufac">Delete Manufacturer</label>
+		
+				<select name="manufac">
+					<?php 
+						$manufac = $conn->query("SELECT * FROM manufacturer_info");
+						while(list($_mid,$_mname) = $manufac->fetch_row()){
+							echo "<option value='$_mid'>$_mname</option>";
+						}
+					?>
+				</select>
+			
+		
+		
+		<input type="submit" name="delmanufact" value="delete manufacturer" />
+		
+</form>
+
+<!-- viewing prodouct in the table   -->
+ 
+<h3>View Product Detals </h3>
+<table border="1" style="border-collapse:collapse">
+    <tr>
+        <td>Manufacturer Name</td>
+        <td>Manufacturer Address</td>
+        <td>Manufacturer Contact</td>
+        <td>Product Name</td>
+        <td>Product Price</td>
+    
+    </tr>
+    <?php
+    $show_all = $conn->query("SELECT * FROM all_table_info");
+    while(list ($_manuf_name,$_manuf_add,$_manufac_cont,$_prod_name,$_prod_price)= $show_all->fetch_row()){ 
+    echo "<tr> 
+        <td> $_manuf_name</td>
+        <td> $_manuf_add</td>
+        <td> $_manufac_cont</td>
+        <td> $_prod_name</td>
+        <td> $_prod_price</td>
+    
+    </tr>";
+    
+    };
+    ?>
+</table>
 
 </body>
 </html>
